c8ba80c7bc032259d5f6d04f8ec7724abb0746f0
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-10-12 12:04:09 -0500

    Update inputtype.js

--- inputtype.js
+++ inputtype.js
@@ -1,3 +1,18 @@
 var x = prompt("please value of X :");
 var recup = typeof x;
 alert("you type a : " + " " + recup);
+
+
+// just for fun 
+var x = prompt("please value of X :");
+var recup = typeof x;
+alert("you type a : " + " " + recup);
+
+var obj ={ 
+           qui:"Nous sommes",
+           ou :" Facebook frontend Engineers",
+           affiche : function(){
+              alert(this.qui + " " + this.ou);
+           }
+         };
+    obj.affiche();
